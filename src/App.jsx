31054b57e4162d1ebb77b691384b46631b84d7bc
imnishalmc <nishalamichhane254@gmail.com>
2026-01-09 23:20:34 +0545

    next change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { TodoProvider } from './context/TodoContext'
+
 import './App.css'
 
 function App() {
+const [todo, setTodo]= useState([])
+
+const addTodo =(id)=>{
+
+}
+
 
   return (
-    <>
-    </>
+    <TodoProvider value={{todo,addTodo,deleteTodo,updateTodo,toggleTodo}}>
+    <div className='bg-slate-400 min-h-screen py-20'>
+ <div className='w-full max-w-2xl mx-auto rounded-3xl px-4 text-white '>
+  <h1 className='text-2xl font-bold text-center mb-3 mt-3'>
+    Manage your TODOS
+  </h1>
+  <div className='mb-4'>
+
+  </div>
+  <div className='flex flex-wrap gap-y-2'>
+
+  </div>
+
+ </div>
+
+
+
+    </div>
+    </TodoProvider>
   )
 }
 
--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -8,15 +8,30 @@ todo:[
         title: "read books",
         completed:false,
     }
-]
+],
+addTodo: (title)=>{},
+updateTodo :(id,title)=>{},
+deleteTodo : (id)=>{},
+toggleTodo:(id)=>{}
 })
-//you can directly export it and such type 
+
+//this structure basically define , create context's initial value is in 
+//object format and inside that object , the value of first key is array
+//also the arrau contain the object that define the property of the data
+
+// in a very simple way of understanding , it mainly holds the things that are 
+//used in the project,like the actual todo or lets say data is used , the operation of the todo 
+//like delete,update,add,toogle and all
+
+
+// so this context define that these data and action exists but here the actual functional logoc is not mention /written , it is done in the app.jsx
+
+//you can directly export TodoContextand such type 
 //of export is called the named export . to import this you have to use 
 //{} this  while importing 
 
-
-export const useTodo = ()=>{
-    return  useContext(TodoContext)
+export const useTodo=()=>{
+    return useContext(TodoContext)
 }
 export const TodoProvider =TodoContext.Provider
 
